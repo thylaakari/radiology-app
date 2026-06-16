9b983fae0875975e4a52df7d64e7da2d2cb71f17
Lev Vorobev <thylaakari@yandex.ru>
2026-06-16 20:20:53 +0500

    rel

--- app/composables/useDb.ts
+++ app/composables/useDb.ts
@@ -4,7 +4,7 @@ class AppDatabase extends Dexie {
   constructor() {
     super('radiology-hub')
 
-    this.version(7).stores({
+    this.version(8).stores({
       patients:
         'id, shortName, firstName, middleName, lastName, birthDate, gender, createdAt, updatedAt, deleted',
       reports:
